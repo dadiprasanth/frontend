a890f69f5147a4b8e23b05f6693c78582f991c22
Yogesh13-waran <yogeshwaran130299@gmail.com>
2022-09-21 13:40:06 +0530

    yogeshuploads

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 
 import Addproperty from './components/addproperty/addproperty';
 import './App.css';
+import Login from './components/Authentication/login';
+import Register from './components/Authentication/register';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+
 
 
 function App() {
@@ -9,6 +13,12 @@ function App() {
      <>
      {/* //place your routing elements here  u can see if i add routoing here i will give this part*/}
     <Addproperty/>
+    <BrowserRouter>
+<Routes>
+  <Route path="/" element={<Register/>}/>
+  <Route path="/login" element={<Login/>}/>
+
+  </Routes></BrowserRouter>
     </>
   );
 }
---
+++ src/components/Authentication/login.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react"
+// import { Icon } from 'react-icons-kit';
+// import {eyeOff} from 'react-icons-kit/feather/eyeOff';
+// import {eye} from 'react-icons-kit/feather/eye';
+ import { useNavigate } from "react-router-dom";
+import "./login.css"
+const Login=()=>{
+    const [input,setInput]=useState("")
+    // const [type,settype]=useState("password")
+    // const [icon,setIcon]=useState(eyeOff)
+    // const Toggle=()=>{
+    //     if(type==="password"){
+    //         setIcon(eye);
+    //         settype("text")
+    //     }
+    //     else{
+    //         setIcon(eyeOff);
+    //         settype("password")
+    //     }
+    // }
+
+    let nav=useNavigate()
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        console.log(input)
+       nav("/")
+    }
+    return(
+        <>
+        <div className="login-form">
+            <div>
+          <p className="logo">Logo</p>
+          <p className="text">Enter your credentials to access your account</p></div>
+          <form className="login-data"
+          onSubmit={handleSubmit}
+          >
+          
+           <input
+           className="userdata"
+           name="userid"
+        //    type={"email"}
+           required
+           placeholder="User ID"
+           onChange={(e)=>{setInput({...input,userid:e.target.value})}}
+           />
+
+
+           <input 
+           required
+        //    type={type}
+           placeholder="Password"
+           onChange={(e)=>{setInput({...input,password:e.target.value})}}
+
+           />
+
+
+<button type="sumbit">Sign In</button>
+
+          </form>
+         
+        </div>
+
+        </>
+    )
+}
+export default Login
+ 
