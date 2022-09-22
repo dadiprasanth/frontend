66a7bed8c46ac937d018e9fd01305ef7d587edff
dadiprasanth <dadiprasanth24@gmail.com>
2022-09-22 08:47:37 +0530

    added logout option

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,34 @@
+import { useState } from "react";
+import {useNavigate} from "react-router-dom"
 const Header=()=>{
+    const nav=useNavigate()
+    const[pop,setpop]=useState(false)
+    const popfun=()=>{
+        if(!pop){
+            return(
+                <><i class="fa fa-arrow-down" aria-hidden="true"></i></>
+            )
+        }else{
+            return(
+                <><i class="fa fa-arrow-up" aria-hidden="true"></i>
+                <div class="pop" onClick={()=>nav("/login")}>logout</div>
+                </>
+            )
+
+        }
+    }
     return(
         <div className="top">
         <header>
             <div>userid :06PPD125</div>
-            <div><i class="fa-solid fa-user"></i> User Name</div>
+            <div>
+            <div>
+                <span onClick={()=>setpop(!pop)}><i class="fa-solid fa-user"></i> UserName 
+                 <span>{popfun()}</span>
+                </span>
+            </div>
+            </div>
+            
         </header>
         </div>
     )
