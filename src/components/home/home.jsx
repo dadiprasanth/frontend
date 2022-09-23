f00a1aac4c62745813ee41422975fa195cb53821
Pankaj-Mali <malipankaj660@gmail.com>
2022-09-23 12:58:05 +0530

    request handling

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -5,7 +5,6 @@ import { useState , useEffect} from "react"
 const Home =(props)=>{
     const [data , setData] = useState([]);
     const {token,settoken}=props;
-    console.log(token)
     useEffect(()=>{
         fetch('http://localhost:8080/property',{
          method:"GET",
